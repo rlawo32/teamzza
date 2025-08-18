8088e06099051dde7eb88150ee9782775fb25f47
sungjae <92013056+rlawo32@users.noreply.github.com>
2025-08-18 17:20:50 +0900

    [Teamzza][FE] <Zustand> TeamStore_작업_20250818 (1)

--- src/app/stores/useShuffleTeamStore.ts
+++ src/app/stores/useShuffleTeamStore.ts
@@ -1,27 +1,30 @@
 import { create } from "zustand";
 
 import useShuffleBaseStore from "./useShuffleBaseStore";
+import useShuffleFixStore from "./useShuffleFixStore";
 
 interface shuffleTeamStore {
-    teams: {id:number, lv:number, nm:string}[][];
-    setTeams: (teams: {id:number, lv:number, nm:string}[][]) => void;
+    teamList: {idx:number, id:string, lv:number, nm:string, tmp:any}[][];
+    setTeamList: (teams: {idx:number, id:string, lv:number, nm:string, tmp:any}[][]) => void;
 
     createTeam: () => void;
-    updateInputData: (data: {index: number; arrNo: number; value: string;}) => void;
-    updateSelectData: (data: {index: number; arrNo: number; value: number;}) => void;
+    shuffleRandom: () => void;
+    shuffleBalance: () => void;
+    updateInputData: (data: {index: number; arrNo: number; input: string;}) => void;
+    updateSelectData: (data: {index: number; arrNo: number; level: number;}) => void;
 }
 
 const useShuffleTeamStore = create<shuffleTeamStore>((set, get) => ({
-    teams: [[]],
-    setTeams: (teams: {id:number, lv:number, nm:string}[][]) =>
-        set((state: {teams: {id:number, lv:number, nm:string}[][]}) => ({
-            teams: (state.teams = teams),
+    teamList: [[]],
+    setTeamList: (teamList: {idx:number, id:string, lv:number, nm:string, tmp:any}[][]) =>
+        set((state: {teamList: {idx:number, id:string, lv:number, nm:string, tmp:any}[][]}) => ({
+            teamList: (state.teamList = teamList),
         })),
     createTeam: () => {
         const {playerCount, teamCount} = useShuffleBaseStore.getState();
         const composition = (playerCount * teamCount) / teamCount;
 
-        let temp2DemList:{id:number, lv:number, nm:string}[][] = Array.from({length: teamCount}, () => Array.from({length: composition}));
+        let tempList:{idx:number, id:string, lv:number, nm:string, tmp:any}[][] = Array.from({length: teamCount}, () => Array.from({length: composition}));
 /*
         if(realPlayerCount % realTeamCount !== 0) {
             let tempComposition:number = Math.ceil(realPlayerCount/realTeamCount);
@@ -49,37 +52,145 @@ const useShuffleTeamStore = create<shuffleTeamStore>((set, get) => ({
             }
         }
 */
-        for(let i=0; i<temp2DemList.length; i++) {
-            let plus:number = 1;
-            for(let j=0; j<temp2DemList[i].length; j++) {
-                temp2DemList[i][j] = {id:i+plus, lv:5, nm:''};
-                plus += teamCount;
+        const idStorage:string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+        let idx:number = 1;
+        for(let i=0; i<tempList.length; i++) {
+            for(let j=0; j<tempList[i].length; j++) {
+                tempList[i][j] = {idx:idx, id:idStorage[i]+'_'+(j+1), lv:5, nm:'', tmp:null};
+                idx += 1;
             }
-        }    
+        }
 
-        set({ teams: temp2DemList });
+        set({ teamList: tempList });
     },
-    updateInputData: (data: {index: number; arrNo: number; value: string;}) => {
-        const state = get(); 
-        const copyTempDataList = JSON.parse(JSON.stringify(state.teams));
-        const index = copyTempDataList[data.arrNo].findIndex((item: {id: number}) => item.id === data.index);
+    shuffleRandom: () => {
+        const copyFixList:{idx:number, id:string, row:number, cell:number, tmp:any}[] = useShuffleFixStore.getState().fixList;
+        const copyTeamList:{idx:number, id:string, lv:number, nm:string, tmp:any}[][] = JSON.parse(JSON.stringify(get().teamList));
+    
+        for(let i=copyTeamList.length-1; i>=0; i--) { 
+            for(let j=copyTeamList[i].length-1; j>=0; j--) {
+                const n = Math.floor(Math.random() * (i+1));
+                const m = Math.floor(Math.random() * (j+1));
+                [copyTeamList[n][j], copyTeamList[i][m]] = [copyTeamList[i][m], copyTeamList[n][j]];
+            }
+        }
+
+        if(copyFixList.length > 0) { 
+            for(let i=0; i<copyFixList.length; i++) {
+                for(let j=0; j<copyTeamList.length; j++) {
+                    for(let x=0; x<copyTeamList[j].length; x++) {
+                        if(j === copyFixList[i].row && x === copyFixList[i].cell) {
+                            const tempBox:{idx:number, id:string, lv:number, nm:string, tmp:any} = copyTeamList[j][x];
+                            let row:number = -1;
+                            let cell:number = -1;
+                            for(let y=0; y<copyTeamList.length; y++) {
+                                cell = copyTeamList[y].findIndex((item) => item.idx === copyFixList[i].idx);
+                                if(cell !== -1) {
+                                    row = y;
+                                    break;
+                                }
+                            }
+                            copyTeamList[j][x] = copyTeamList[row][cell];
+                            copyTeamList[row][cell] = tempBox;
+                        }
+                    }
+                }
+            }
+        }
+
+        set({ teamList: copyTeamList });
+    },
+    shuffleBalance: () => {
+        const copyFixList:{idx:number, id:string, row:number, cell:number, tmp:any}[] = useShuffleFixStore.getState().fixList;
+        const copyTeamList:{idx:number, id:string, lv:number, nm:string, tmp:any}[][] = JSON.parse(JSON.stringify(get().teamList));
+        const {playerCount, teamCount} = useShuffleBaseStore.getState();
+        
+        const temp1DemList:{idx:number, id:string, lv:number, nm:string, tmp:any}[] = [];
+        let temp2DemList:{idx:number, id:string, lv:number, nm:string, tmp:any}[][] = Array.from({length: teamCount}, () => Array.from({length: playerCount}));
+        const tempCompare:{idx:number, sum:number, len:number}[] = [];
+
+        for(let i=0; i<copyTeamList.length; i++) {
+            for(let j=0; j<copyTeamList[i].length; j++) {
+                temp1DemList.push(copyTeamList[i][j]);
+            }
+        }
+
+        for(let i=temp1DemList.length-1; i>=0; i--) {
+            const j = Math.floor(Math.random() * (i+1));
+            [temp1DemList[i], temp1DemList[j]] = [temp1DemList[j], temp1DemList[i]];
+        }
+
+        temp1DemList.sort((a, b) => {
+            return b.lv - a.lv;
+        });
+
+
+        for(let i=0; i<teamCount; i++) {
+            tempCompare[i] = {idx:i, sum:0, len:temp2DemList[i].length};
+        }
+
+        let tmpIdx:number = 0;
+
+        for(let i=0; i<playerCount; i++) {
+            tempCompare.sort((a, b) => {
+                return a.sum - b.sum;
+            });
+            for(let j=0; j<teamCount; j++) {
+                if(temp2DemList[tempCompare[j].idx].length-1 >= i) {
+                    temp2DemList[tempCompare[j].idx][i] = temp1DemList[tmpIdx++];
+                }
+            }
+            for(let j=0; j<teamCount; j++) {
+                if(temp2DemList[tempCompare[j].idx].length-1 >= i) {
+                    tempCompare[j].sum += temp2DemList[tempCompare[j].idx][i].lv;   
+                }
+            }
+        }
+
+        if(copyFixList.length > 0) { 
+            for(let i=0; i<copyFixList.length; i++) {
+                for(let j=0; j<temp2DemList.length; j++) {
+                    for(let x=0; x<temp2DemList[j].length; x++) {
+                        if(j === copyFixList[i].row && x === copyFixList[i].cell) {
+                            const tempBox:{idx:number, id:string, lv:number, nm:string, tmp:any} = temp2DemList[j][x];
+                            let row:number = -1;
+                            let cell:number = -1;
+                            for(let y=0; y<temp2DemList.length; y++) {
+                                cell = temp2DemList[y].findIndex((item) => item.id === copyFixList[i].id);
+                                if(cell !== -1) {
+                                    row = y;
+                                    break;
+                                }
+                            }
+                            temp2DemList[j][x] = temp2DemList[row][cell];
+                            temp2DemList[row][cell] = tempBox;
+                        }
+                    }
+                }
+            }
+        }
+
+        set({ teamList: temp2DemList });
+    },
+    updateInputData: (data: {index: number; arrNo: number; input: string;}) => {
+        const currentTeamList = JSON.parse(JSON.stringify(get().teamList));
+        const index = currentTeamList[data.arrNo].findIndex((item: {idx: number}) => item.idx === data.index);
         
         if (index !== -1) { 
-            copyTempDataList[data.arrNo][index].nm = data.value;
+            currentTeamList[data.arrNo][index].nm = data.input;
         }
 
-        set({ teams: copyTempDataList });
+        set({ teamList: currentTeamList });
     },
-    updateSelectData: (data: {index: number; arrNo: number; value: number;}) => {
-        const state = get(); 
-        const copyTempDataList = JSON.parse(JSON.stringify(state.teams));
-        const index = copyTempDataList[data.arrNo].findIndex((item: {id: number}) => item.id === data.index);
+    updateSelectData: (data: {index: number; arrNo: number; level: number;}) => {
+        const currentTeamList = JSON.parse(JSON.stringify(get().teamList));
+        const index = currentTeamList[data.arrNo].findIndex((item: {idx: number}) => item.idx === data.index);
         
         if (index !== -1) {
-            copyTempDataList[data.arrNo][index].lv = data.value;
+            currentTeamList[data.arrNo][index].lv = data.level;
         }
 
-        set({ teams: copyTempDataList }); 
+        set({ teamList: currentTeamList }); 
     },
 }));
 
