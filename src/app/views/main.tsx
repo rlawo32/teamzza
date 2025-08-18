c9dc04ccc7f2f6695ffa72e3ded0edcc6e0f1eed
sungjae <92013056+rlawo32@users.noreply.github.com>
2025-08-18 17:18:38 +0900

    [Teamzza][FE] <View> MainView_작업_20250818 (2)

--- src/app/views/main.tsx
+++ src/app/views/main.tsx
@@ -11,7 +11,7 @@ import useShuffleBaseStore from "../stores/useShuffleBaseStore";
 
 const Main = () => {
 
-    const { teams, createTeam } = useShuffleArrayStore();
+    const { teamList, createTeam, shuffleRandom, shuffleBalance } = useShuffleTeamStore();
     const { playerCount, teamCount, setPlayerCount, setTeamCount } = useShuffleBaseStore();
     const { fixList } = useShuffleFixStore();
 
@@ -20,39 +20,45 @@ const Main = () => {
     const deletePlayerCount = useShuffleBaseStore((state) => state.deletePlayerCount);
     const deleteTeamCount = useShuffleBaseStore((state) => state.deleteTeamCount);
 
-    const updateInputData = useShuffleArrayStore((state) => state.updateInputData);
-    const updateSelectData = useShuffleArrayStore((state) => state.updateSelectData);
+    const updateInputData = useShuffleTeamStore((state) => state.updateInputData);
+    const updateSelectData = useShuffleTeamStore((state) => state.updateSelectData);
     const updateFixData = useShuffleFixStore((state) => state.updateFixData);
 
     useEffect(() => {
         createTeam();
+        console.log(teamList);
     }, [playerCount, teamCount])
 
     return (
         <Style.MatchShuffle>
             <div className="list_section">
-                {teams.map((parent, idx1) => (
+                {teamList.map((parent, idx1) => (
                     <div key={idx1} className="list_wrap" id={parent.length + "_t"}>
                         <div className="list_parent">
-                            {idx1 === 0 ? 
-                                <div className="team_camp team_blue">
-                                    TeamBlue
-                                </div> 
-                                : 
-                                <div className="team_camp team_red">
-                                    TeamRed
-                                </div>
-                            }
+                            <Style.GroupCampStyle $camp={idx1}>
+                                Team
+                                {
+                                    idx1 === 0 ? 'Blue'     :
+                                    idx1 === 1 ? 'Red'      :
+                                    idx1 === 2 ? 'Yellow'   :
+                                    idx1 === 3 ? 'Green'    :
+                                    idx1 === 4 ? 'Purple'   :
+                                    idx1 === 5 ? 'Brown'    :
+                                    idx1 === 6 ? 'Pink'     : 
+                                    idx1 === 7 ? 'Orange'   :
+                                    idx1 === 8 ? 'Mint'     : 'Lime'
+                                }
+                            </Style.GroupCampStyle>
                             {parent.map((child, idx2) => (
                                 <div key={idx2} className="list_child">
                                     <div className="list_select">
                                         <SelectBox updateSelectData={updateSelectData} inputData={child} inputIdx={idx1} />
                                     </div>
-                                    <Style.InputPlayerStyle onChange={(e) => updateInputData({index:child.id, arrNo:idx1, value:e.target.value})} value={child.nm} 
+                                    <Style.InputPlayerStyle onChange={(e) => updateInputData({index:child.idx, arrNo:idx1, input:e.target.value})} value={child.nm} 
                                                 type="text" id={"input_" + child.id} $camp={idx1}/>
                                     <div className="list_check">
-                                        <Style.CheckStyle onChange={(e) => updateFixData({checked:e.target.checked, index:child.id, arrNo:idx1, value:idx2})} 
-                                                    checked={fixList.some(data => data.id === child.id) ? true : false} type="checkbox" id={"chkbx" + child.id} />
+                                        <Style.CheckStyle onChange={(e) => updateFixData({checked:e.target.checked, index:child.idx, id:child.id, arrNo:idx1, value:idx2, tmp:null})} 
+                                                    checked={fixList.some(data => data.idx === child.idx) ? true : false} type="checkbox" id={"chkbx" + child.id} />
                                         <Style.LabelStyle htmlFor={"chkbx" + child.id} className="check_box">
                                             고정
                                         </Style.LabelStyle>
@@ -67,6 +73,8 @@ const Main = () => {
             <button onClick={() => insertPlayerCount()}>그룹원 추가</button>
             <button onClick={() => deleteTeamCount()}>그룹 삭제</button>
             <button onClick={() => deletePlayerCount()}>그룹원 삭제</button>
+            <button onClick={() => shuffleRandom()}>무작위 셔플</button>
+            <button onClick={() => shuffleBalance()}>밸런스 셔플</button>
         </Style.MatchShuffle>
     )
 }
