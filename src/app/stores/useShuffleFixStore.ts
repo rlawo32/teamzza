971870c453f0a0abf0e4341d7b4baef0a1323696
sungjae <92013056+rlawo32@users.noreply.github.com>
2025-08-19 17:38:17 +0900

    [Teamzza][FE] <Zustand> FixStore_작업_20250819 (2)

--- src/app/stores/useShuffleFixStore.ts
+++ src/app/stores/useShuffleFixStore.ts
@@ -1,10 +1,16 @@
 import { create } from "zustand";
 
+import useShuffleBaseStore from "./useShuffleBaseStore";
+
 interface shuffleFixStore {
     fixList: {idx:number, id:string, row:number, cell:number, tmp:any}[];
     setFixList: (fixList: {idx:number, id:string, row:number, cell:number, tmp:any}[]) => void;
+    tempList: {id:string, idx:number, arr:{idx:number, id:string, lv:number, nm:string, tmp:any}[][]}[];
+    setTempList: (tempList: {id:string, idx:number, arr:{idx:number, id:string, lv:number, nm:string, tmp:any}[][]}[]) => void;
     
     updateFixData: (data: {checked:boolean; index:number; id:string, arrNo:number; value:number; tmp:any;}) => void;
+    updateTempData: (data: {idx:number, id:string, lv:number, nm:string, tmp:any}[][]) => void;
+    deleteTempData: () => void;
 }
 
 const useShuffleFixStore = create<shuffleFixStore>((set, get) => ({
@@ -12,6 +18,11 @@ const useShuffleFixStore = create<shuffleFixStore>((set, get) => ({
     setFixList: (fixList: {idx:number, id:string, row:number, cell:number, tmp:any}[]) =>
         set((state: {fixList: {idx:number, id:string, row:number, cell:number, tmp:any}[]}) => ({
             fixList: (state.fixList = fixList),
+        })),
+    tempList: [],
+    setTempList: (tempList: {id:string, idx:number, arr:{idx:number, id:string, lv:number, nm:string, tmp:any}[][]}[]) =>
+        set((state: {tempList: {id:string, idx:number, arr:{idx:number, id:string, lv:number, nm:string, tmp:any}[][]}[]}) => ({
+            tempList: (state.tempList = tempList),
         })),
     updateFixData: (data: {checked:boolean; index:number; id:string; arrNo:number; value:number;}) => {
         if (data.checked) {
@@ -25,6 +36,24 @@ const useShuffleFixStore = create<shuffleFixStore>((set, get) => ({
                 fixList: currentFixList.filter((el) => el.idx !== data.index),
             });
         }
+    },
+    updateTempData: (data: {idx:number, id:string, lv:number, nm:string, tmp:any}[][]) => {
+        const {rollbackCount, insertRollbackCount} = useShuffleBaseStore.getState();
+        const currentTempList = get().tempList;
+        set({
+            tempList: [...currentTempList, { id:'rb_'+rollbackCount, idx:rollbackCount, arr:data }],
+        });
+        insertRollbackCount();
+    },
+    deleteTempData: () => {
+        const {deleteRollbackCount} = useShuffleBaseStore.getState();
+        const currentTempList = get().tempList;
+        if(currentTempList.length > 0) {
+            set({ 
+                tempList: currentTempList.slice(0, -1) 
+            });
+        }
+        deleteRollbackCount();
     },
 }));
 
