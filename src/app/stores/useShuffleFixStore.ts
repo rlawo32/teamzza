f8222331ae50543933ec4abfbd634cd58c79528f
sungjae <92013056+rlawo32@users.noreply.github.com>
2025-08-22 17:52:22 +0900

    [Teamzza][FE] <Zustand> FixStore_작업_20250822 (5)

--- src/app/stores/useShuffleFixStore.ts
+++ src/app/stores/useShuffleFixStore.ts
@@ -3,43 +3,64 @@ import { create } from "zustand";
 import useShuffleBaseStore from "./useShuffleBaseStore";
 
 interface shuffleFixStore {
+    autoList: {cnt:number, raw:string};
+    setAutoList: (autoList: {cnt:number, raw:string}) => void;
     fixList: {idx:number, id:string, row:number, cell:number, tmp:any}[];
     setFixList: (fixList: {idx:number, id:string, row:number, cell:number, tmp:any}[]) => void;
-    rollbackList: {id:string, idx:number, arr:{idx:number, id:string, lv:number, nm:string, tmp:any}[][]}[];
-    setRollbackList: (rollbackList: {id:string, idx:number, arr:{idx:number, id:string, lv:number, nm:string, tmp:any}[][]}[]) => void;
+    rollbackList: {id:string, idx:number, arr:{title:string, target:boolean, list: {idx:number, id:string, lv:number, nm:string, tmp:any}[]}[]}[];
+    setRollbackList: (rollbackList: {id:string, idx:number, arr:{title:string, target:boolean, list: {idx:number, id:string, lv:number, nm:string, tmp:any}[]}[]}[]) => void;
     
+    updateAutoData: (data: {type:string, value:any}) => void;
     updateFixData: (data: {checked:boolean; index:number; id:string, arrNo:number; value:number; tmp:any;}) => void;
-    updateRollbackData: (data: {idx:number, id:string, lv:number, nm:string, tmp:any}[][]) => void;
+    updateRollbackData: (data: {title:string, target:boolean, list: {idx:number, id:string, lv:number, nm:string, tmp:any}[]}[]) => void;
     deleteRollbackData: () => void;
 }
 
 const useShuffleFixStore = create<shuffleFixStore>((set, get) => ({
+    autoList: {cnt:5, raw:''},
+    setAutoList: (autoList: {cnt:number, raw:string}) =>
+        set((state: {autoList: {cnt:number, raw:string}}) => ({
+            autoList: (state.autoList = autoList),
+        })),
+    updateAutoData: (data: {type:string, value:any}) => {
+        const currentAutoList:{cnt:number, raw:string} = get().autoList;
+        if (data.type === 'cnt') {
+            currentAutoList.cnt = data.value;
+            set({
+                autoList: currentAutoList,
+            });
+        } else {
+            currentAutoList.raw = data.value;
+            set({
+                autoList: currentAutoList,
+            });
+        }
+    },
     fixList: [],
     setFixList: (fixList: {idx:number, id:string, row:number, cell:number, tmp:any}[]) =>
         set((state: {fixList: {idx:number, id:string, row:number, cell:number, tmp:any}[]}) => ({
             fixList: (state.fixList = fixList),
         })),
-    rollbackList: [],
-    setRollbackList: (rollbackList: {id:string, idx:number, arr:{idx:number, id:string, lv:number, nm:string, tmp:any}[][]}[]) =>
-        set((state: {rollbackList: {id:string, idx:number, arr:{idx:number, id:string, lv:number, nm:string, tmp:any}[][]}[]}) => ({
-            rollbackList: (state.rollbackList = rollbackList),
-        })),
     updateFixData: (data: {checked:boolean; index:number; id:string; arrNo:number; value:number;}) => {
+        const currentFixList: {idx:number, id:string, row:number, cell:number, tmp:any}[] = get().fixList;
         if (data.checked) {
-            const currentFixList = get().fixList;
             set({
                 fixList: [...currentFixList, { idx: data.index, id: data.id, row: data.arrNo, cell: data.value, tmp:null }],
             });
         } else {
-            const currentFixList = get().fixList;
             set({
                 fixList: currentFixList.filter((el) => el.idx !== data.index),
             });
         }
     },
-    updateRollbackData: (data: {idx:number, id:string, lv:number, nm:string, tmp:any}[][]) => {
+    rollbackList: [],
+    setRollbackList: (rollbackList: {id:string, idx:number, arr:{title:string, target:boolean, list: {idx:number, id:string, lv:number, nm:string, tmp:any}[]}[]}[]) =>
+        set((state: {rollbackList: {id:string, idx:number, arr:{title:string, target:boolean, list: {idx:number, id:string, lv:number, nm:string, tmp:any}[]}[]}[]}) => ({
+            rollbackList: (state.rollbackList = rollbackList),
+        })),
+    updateRollbackData: (data: {title:string, target:boolean, list: {idx:number, id:string, lv:number, nm:string, tmp:any}[]}[]) => {
         const {rollbackCount, increaseRollbackCount} = useShuffleBaseStore.getState();
-        const currentRollbackList = get().rollbackList;
+        const currentRollbackList:{id:string, idx:number, arr:{title:string, target:boolean, list: {idx:number, id:string, lv:number, nm:string, tmp:any}[]}[]}[] = get().rollbackList;
         set({
             rollbackList: [...currentRollbackList, { id:'rb_'+(rollbackCount+1), idx:rollbackCount+1, arr:data }],
         });
@@ -47,7 +68,7 @@ const useShuffleFixStore = create<shuffleFixStore>((set, get) => ({
     },
     deleteRollbackData: () => {
         const {decreaseRollbackCount} = useShuffleBaseStore.getState();
-        const currentRollbackList = get().rollbackList;
+        const currentRollbackList:{id:string, idx:number, arr:{title:string, target:boolean, list: {idx:number, id:string, lv:number, nm:string, tmp:any}[]}[]}[] = get().rollbackList;
         if(currentRollbackList.length > 0) {
             set({ 
                 rollbackList: currentRollbackList.slice(0, -1) 
