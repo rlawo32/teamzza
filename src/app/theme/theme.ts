cea90fbba09bbc609a50f13737ccfc6c160a9d86
sungjae <92013056+rlawo32@users.noreply.github.com>
2025-08-26 17:37:54 +0900

    [Teamzza][FE] <CSS> 다크모드_theme_설정_20250826 (2)

---
+++ src/app/theme/theme.ts
@@ -0,0 +1,30 @@
+export const lightTheme = {
+    bgColor: "",
+    reverseBgColor: "",
+    textColor: "",
+    reverseTextColor: "",
+    inputBgColor: "",
+    inputTextColor: "",
+    buttonBgColor: "",
+    buttonTextColor: "",
+    boxBgColor: "",
+    borderColor: "",
+};
+
+export const darkTheme = {
+    bgColor: "",
+    reverseBgColor: "",
+    textColor: "",
+    reverseTextColor: "",
+    inputBgColor: "",
+    inputTextColor: "",
+    buttonBgColor: "",
+    buttonTextColor: "",
+    boxBgColor: "",
+    borderColor: "",
+};
+
+export const theme = {
+    darkTheme,
+    lightTheme,
+};
