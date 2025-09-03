494b8a46bcc70ef6b7a6bf8cb330bd031250c7c3
sungjae <92013056+rlawo32@users.noreply.github.com>
2025-09-03 11:18:37 +0900

    [Teamzza][FE] <CSS> font_오류_작업_20250903 (1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ config.autoAddCss = false
 
 // font-family addition
 const pretendard = localFont({
-  src: "./fonts/PretendardVariable.woff2",
+  src: "/fonts/PretendardVariable.woff2",
   display: "swap",
   weight: "45 920",
   variable: "--font-pretendard",
