82320b396df4345e57c86782a5235547b9d3fda5
rlawo <rlawo32@naver.com>
2025-09-06 19:59:48 +0900

    [Teamzza][FE] <SEO> Metadata_icon_추가_20250906

--- src/app/metadata.tsx
+++ src/app/metadata.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
                 직관적인 UI와 편리한 기능을 통해 사용자 맞춤형 팀 조합이 가능하며,  
                 무작위 또는 사용자가 설정한 밸런스에 따라 팀을 분리하여 구성해주는 서비스입니다.`,   
   icons: {
-    icon: '/vercel.svg',
+    icon: '/dice.png',
   },
   alternates: {
     canonical: 'https://teamzza.co.kr/',
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     siteName: 'Teamzza - 팀짜',
     images: [
       {
-        url: '/vercel.svg',
+        url: '/dice.png',
         width: 1200,
         height: 630,
         alt: 'Teamzza OG Image',
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: 'Teamzza - 팀짜',
     description: '편리한 UI와 기능으로 팀을 구성하세요!',
-    images: ['/vercel.svg'],
+    images: ['/dice.png'],
   },
   robots: {
     index: true,
