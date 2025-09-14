9029f1f1c9e412b3830c9e6e43ac0d21e20870a3
rlawo <rlawo32@naver.com>
2025-09-15 00:56:53 +0900

    [Teamzza][FE] <SEO> Icon&Title_변경_20250915

--- src/app/metadata.tsx
+++ src/app/metadata.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://teamzza.co.kr'),
-  title: '팀 만들기 | 팀짜',
+  title: '팀 만들기, 팀 짜기 | 팀짜',
   description: `플레이어 입력만으로 손쉽게 다양한 방식의 팀 구성이 가능합니다.  
                 직관적인 UI와 편리한 기능을 통해 사용자 맞춤형 팀 조합이 가능하며,  
                 무작위 또는 사용자가 설정한 밸런스에 따라 팀을 분리하여 구성해주는 서비스입니다.`,   
@@ -13,10 +13,10 @@ export const metadata: Metadata = {
     canonical: 'https://teamzza.co.kr/',
   },
   openGraph: {
-    title: '팀 만들기 | 팀짜',
+    title: '팀 만들기, 팀 짜기 | 팀짜',
     description: `편리한 UI와 기능으로 팀을 구성하세요!`,
     url: 'https://teamzza.co.kr/',
-    siteName: '팀 만들기 | 팀짜',
+    siteName: '팀 만들기, 팀 짜기 | 팀짜',
     images: [
       {
         url: '/icon.png',
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: '팀 만들기 | 팀짜',
+    title: '팀 만들기, 팀 짜기 | 팀짜',
     description: '편리한 UI와 기능으로 팀을 구성하세요!',
     images: ['/icon.png'],
   },
