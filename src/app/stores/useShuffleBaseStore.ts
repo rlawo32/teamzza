bf8e82b380af88d35b0c9faf97e04bbef338056e
sungjae <92013056+rlawo32@users.noreply.github.com>
2025-10-16 17:55:17 +0900

    [Teamzza][FE] <Zustand> BaseStore_Mode_기능_추가_20251016 (1)

--- src/app/stores/useShuffleBaseStore.ts
+++ src/app/stores/useShuffleBaseStore.ts
@@ -1,11 +1,22 @@
 import { create } from "zustand";
 
+import useShuffleTeamStore from "./useShuffleTeamStore";
+
 interface shuffleBaseStore {
     teamTitleStorage: string[];
     teamIdStorage: string[];
 
     themeMode: boolean;
     setThemeMode: (themeMode: boolean) => void;
+
+    /*
+        LOL - TOP, JUG, MID, ADC, SUP
+        BattleGround - tier 1, tier 2, tier 3, tier 4
+        OverWatch - Tank, Deal, Deal, Heal, Heal
+        Valorant - 
+    */
+    shuffleModeStorage: {id:string, target:boolean, cnt:number, fix:boolean, list:{idx:number, lv:number, tmp:string}[]}[];
+    updateModeTarget: (data: {id: string; target: boolean;}) => void;
 
     shuffleProgress: boolean;
     setShuffleProgress: (shuffleProgress: boolean) => void;
@@ -58,6 +69,75 @@ const useShuffleBaseStore = create<shuffleBaseStore>((set, get) => ({
         set((state: {themeMode: boolean}) => ({
             themeMode: (state.themeMode = themeMode),
         })),
+    shuffleModeStorage: [
+        {id:'D', target:true, cnt:5, fix:false, list:[]},
+        {id:'L', target:false, cnt:5, fix:true, list:[{idx:1, lv:5, tmp:'TOP'}, {idx:2, lv:4, tmp:'JUG'}, {idx:3, lv:3, tmp:'MID'}, {idx:4, lv:2, tmp:'ADC'}, {idx:5, lv:1, tmp:'SUP'}]},
+        {id:'B', target:false, cnt:4, fix:true, list:[{idx:1, lv:4, tmp:'1 Tier'}, {idx:2, lv:3, tmp:'2 Tier'}, {idx:3, lv:2, tmp:'3 Tier'}, {idx:4, lv:1, tmp:'4 Tier'}]},
+        {id:'O', target:false, cnt:5, fix:false, list:[{idx:1, lv:3, tmp:'Tank'}, {idx:2, lv:2, tmp:'Deal'}, {idx:3, lv:1, tmp:'Heal'}]},
+        {id:'V', target:false, cnt:5, fix:false, list:[{idx:1, lv:5, tmp:'1 Tier'}, {idx:2, lv:4, tmp:'2 Tier'}, {idx:3, lv:3, tmp:'3 Tier'}, {idx:4, lv:2, tmp:'4 Tier'}, {idx:5, lv:1, tmp:'5 Tier'}]},
+    ], // Default, LOL, BattleGround, OverWatch, Valorant
+    updateModeTarget: (data: {id: string; target: boolean;}) => {
+        const {teamList, setTeamList} = useShuffleTeamStore.getState();
+        const currentShuffleModeStorage:{id:string, target:boolean, cnt:number, fix:boolean, list:{idx:number, lv:number, tmp:string}[]}[] = JSON.parse(JSON.stringify(get().shuffleModeStorage));
+        
+        const result = currentShuffleModeStorage.find((item) => item.id === data.id);
+
+        if(result) {
+            result.target = data.target;
+
+            currentShuffleModeStorage.filter((item) => item.id !== data.id).forEach((item) => {
+                item.target = false;
+            });
+            set({shuffleModeStorage: currentShuffleModeStorage});
+
+            let idx:number = 1;
+            let newTeamList;
+            if(result.id !== 'D') {
+                set({playerCount: result.cnt});
+
+                newTeamList = teamList.map((team, teamIdx) => {
+                    const len:number = Math.abs(result.cnt - team.list.length);
+                    if(result.cnt < team.list.length) {
+                        team.list = team.list.slice(0, -len);
+                    } else {
+                        for(let i:number=0; i<len; i++) {
+                            team.list.push({idx:0, id:get().teamIdStorage[teamIdx]+'_'+(team.list.length+1), lv:0, nm:'', tmp:0, as:''});
+                        }
+                    }
+                    for(let i:number=0; i<team.list.length; i++) {
+                        team.list[i].idx = idx;
+                        if(result.id === 'O') {
+                            team.list[i].lv = result.list[i > 1 ? i > 2 ? 2 : 1 : i].lv;
+                            team.list[i].tmp = result.list[i > 1 ? i > 2 ? 2 : 1 : i].tmp;
+                            team.list[i].as = '';
+                        } else {
+                            team.list[i].lv = result.list[i].lv;
+                            team.list[i].tmp = result.list[i].tmp;
+                            team.list[i].as = result.id === 'L' ? '' : '';
+                        }
+                        idx += 1;
+                    }
+
+                    return {title:team.title, target:team.target, list:team.list}; 
+                });
+            } else {
+                newTeamList = teamList.map((team, teamIdx) => {
+                    for(let i:number=0; i<team.list.length; i++) {
+                        team.list[i].idx = idx;
+                        team.list[i].lv = (get().playerCount)-i;
+                        team.list[i].tmp = (get().playerCount)-i;
+                        team.list[i].as = 'LEVEL';
+                        idx += 1;
+                    }
+
+                    return {title:team.title, target:team.target, list:team.list}; 
+                });
+            }
+
+            setTeamList(newTeamList);
+        }
+        
+    },
     shuffleProgress: false,
     setShuffleProgress: (shuffleProgress: boolean) =>
         set((state: {shuffleProgress: boolean}) => ({
